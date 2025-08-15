e367b4fef6ac8209d8361ce022cd23745a91508f
Samrat Bhattarai <samratbhattarai6789@gmail.com>
2025-08-15 07:58:35 +0545

    Fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-	base: "/EventJhapa/", // EXACT repo name
+	base: "/", // EXACT repo name
 	plugins: [react()],
 });
