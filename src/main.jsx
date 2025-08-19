1b5967cb9f2dad99fad902a3cd53d3a72a5db76b
Samrat Bhattarai <samratbhattarai6789@gmail.com>
2025-08-19 14:56:29 +0545

    fixing bugs using  HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import App from "./App";
 import "slick-carousel/slick/slick.css"; //  important
 import "slick-carousel/slick/slick-theme.css"; // important
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<React.StrictMode>
+		<HashRouter>
+			<App />
+		</HashRouter>
+	</React.StrictMode>
 );
